6cca1dac4f4ffc21511df21547715eb7cacb56fc
TruongPhuocHung <insect228060132902060400006417@gmail.com>
2025-05-27 08:40:52 +0700

    feat: add AddContactForm

--- ContactList/ContactList/MainForm.Designer.cs
+++ ContactList/ContactList/MainForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnThemLienHe = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,21 +41,22 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Danh sách liên hệ";
             // 
-            // button1
+            // btnThemLienHe
             // 
-            this.button1.Location = new System.Drawing.Point(330, 100);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Thêm liên hệ";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnThemLienHe.Location = new System.Drawing.Point(330, 100);
+            this.btnThemLienHe.Name = "btnThemLienHe";
+            this.btnThemLienHe.Size = new System.Drawing.Size(134, 23);
+            this.btnThemLienHe.TabIndex = 1;
+            this.btnThemLienHe.Text = "Thêm liên hệ";
+            this.btnThemLienHe.UseVisualStyleBackColor = true;
+            this.btnThemLienHe.Click += new System.EventHandler(this.btnThemLienHe_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnThemLienHe);
             this.Controls.Add(this.label1);
             this.Name = "MainForm";
             this.Text = "MainForm";
@@ -67,7 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnThemLienHe;
     }
 }
 
